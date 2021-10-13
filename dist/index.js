6401127158e8209a69876af5e2265e860ce2be5a
osaid202 <osaid202@gmail.com>
2021-10-13 05:09:24 -0500

    import_fixed_use_inspect!

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import './style.css'
+//import './style.css';
 //import * as THREE from 'three';
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.133.1/build/three.module.js';
 import {OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js';
